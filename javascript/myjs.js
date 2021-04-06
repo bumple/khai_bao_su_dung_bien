51c13c4d98de254ed6244637b0993670e2c67690
bumple <81680183+bumple@users.noreply.github.com>
2021-04-06 16:52:07 +0700

    first commit

---
+++ javascript/myjs.js
@@ -0,0 +1,6 @@
+let a = +prompt("nhập số thứ nhất")
+let b = +prompt("nhập số thứ hai")
+let x = a % b
+if (x===0){alert(a + " là bội số của " + b)
+} else {alert(a + " không phải là bội số của " + b)
+}
